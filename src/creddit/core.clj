656bc813f85272a21e840b0b4a1508aacc9aaa5b
dimovich <dimovich@gmail.com>
2019-10-13 23:03:44 +0300

    use oauth; use simple fns instead of protocol

--- src/creddit/core.clj
+++ src/creddit/core.clj
@@ -1,71 +1,279 @@
 (ns creddit.core
-  (:require [creddit.client :as client]))
+  (:require [looper.client :as client]
+            [clojure.string :as string]
+            [jsonista.core :as json]
+            [slingshot.slingshot :refer [try+]]))
 
-(defprotocol RedditApi
-  (frontpage [this limit time])
-  (controversial [this limit time])
-  (new [this limit time])
-  (rising [this limit time])
-  (top [this limit time])
-  (subreddit [this subreddit limit time])
-  (subreddit-controversial [this subreddit limit time])
-  (subreddit-new [this subreddit limit time])
-  (subreddit-rising [this subreddit limit time])
-  (subreddit-top [this subreddit limit time])
-  (subreddit-comments [this subreddit limit])
-  (subreddit-search [this subreddit query limit])
-  (subreddit-about [this subreddit])
-  (subreddit-moderators [this subreddit])
-  (subreddits [this limit])
-  (subreddits-new [this limit])
-  (subreddits-popular [this limit])
-  (subreddits-gold [this limit])
-  (subreddits-default [this limit])
-  (subreddits-search [this subreddit limit])
-  (user [this username])
-  (user-trophies [this username])
-  (user-posts [this username limit time])
-  (user-comments [this username limit time])
-  (users [this limit])
-  (users-new [this limit])
-  (users-popular [this limit])
-  (listing [this names]))
 
-(defrecord CredditClient [credentials]
-  RedditApi
-  (frontpage [this limit time] (client/frontpage credentials limit time))
-  (controversial [this limit time] (client/controversial credentials limit time))
-  (new [this limit time] (client/new credentials limit time))
-  (rising [this limit time] (client/rising credentials limit time))
-  (top [this limit time] (client/top credentials limit time))
-  (subreddit [this subreddit limit time] (client/subreddit credentials subreddit limit time))
-  (subreddit-controversial [this subreddit limit time] (client/subreddit-controversial credentials subreddit limit time))
-  (subreddit-new [this subreddit limit time] (client/subreddit-new credentials subreddit limit time))
-  (subreddit-rising [this subreddit limit time] (client/subreddit-rising credentials subreddit limit time))
-  (subreddit-top [this subreddit limit time] (client/subreddit-top credentials subreddit limit time))
-  (subreddit-comments [this subreddit limit] (client/subreddit-comments credentials subreddit limit))
-  (subreddit-search [this subreddit query limit] (client/subreddit-search credentials subreddit query limit))
-  (subreddit-about [this subreddit] (client/subreddit-about credentials subreddit))
-  (subreddit-moderators [this subreddit] (client/subreddit-moderators credentials subreddit))
-  (subreddits [this limit] (client/subreddits credentials limit))
-  (subreddits-new [this limit] (client/subreddits-new credentials limit))
-  (subreddits-popular [this limit] (client/subreddits-popular credentials limit))
-  (subreddits-gold [this limit] (client/subreddits-gold credentials limit))
-  (subreddits-default [this limit] (client/subreddits-default credentials limit))
-  (subreddits-search [this subreddit limit] (client/subreddits-search credentials subreddit limit))
-  (user [this username] (client/user credentials username))
-  (user-trophies [this username] (client/user-trophies credentials username))
-  (user-posts [this username limit time] (client/user-posts credentials username limit time))
-  (user-comments [this username limit time] (client/user-comments credentials username limit time))
-  (users [this limit] (client/users credentials limit))
-  (users-new [this limit] (client/users-new credentials limit))
-  (users-popular [this limit] (client/users-popular credentials limit))
-  (listing [this names] (client/listing credentials names)))
+(def json-mapper (json/object-mapper
+                  {:decode-key-fn true}))
+
+
+(defn- parse-response
+  [response]
+  (if-let [coll (or (get-in response [:data :children])
+                    (get-in response [:data :trophies]))]
+    (map :data coll)
+    (:data response)))
+
+(defn- valid-limit? [limit]
+  (if (and (integer? limit)
+           (<= 1 limit)
+           (>= 100 limit))
+    limit
+    (throw
+      (ex-info "Invalid limit - Must be an integer between 1 & 100."
+               {:causes :invalid-limit}))))
+
+(defn- valid-time? [time]
+  (if (and (keyword? time)
+           (contains? #{:hour :day :week :month :year :all} time))
+    time
+    (throw
+      (ex-info "Invalid time - Must be one of the following: :hour, :day, :week, :month, :year, :all."
+               {:causes :invalid-time}))))
+
+(defn get-access-token
+  [credentials]
+  (try+
+   (-> (client/post "https://www.reddit.com/api/v1/access_token"
+                    {:basic-auth [(:user-client credentials) (:user-secret credentials)]
+                     :headers {"User-Agent" (or (:user-agent credentials) "creddit")}
+                     :form-params {:grant_type "client_credentials"
+                                   :device_id (str (java.util.UUID/randomUUID))}
+                     :content-type "application/x-www-form-urlencoded"
+                     ;;:socket-timeout 10000
+                     ;;:conn-timeout 10000
+                     })
+       (get :body)
+       (json/read-value json-mapper))
+   
+   (catch [:status 401] {}
+     (throw
+      (ex-info "Unauthorised, please check your credentials are correct."
+               {:causes :unauthorised})))))
+
+
+
+(defn- http-req [method credentials url & [opts]]
+  (-> (method url
+              (merge
+               {:headers {"User-Agent" (:user-agent credentials)
+                          "Authorization" (str "bearer " (:access-token credentials))}}
+               opts))
+      :body
+      (json/read-value json-mapper)))
+
+
+(defn- http-get [credentials url & [opts]]
+  (http-req client/get credentials url opts))
+
+
+(defn- http-post [credentials url & [opts]]
+  (http-req client/post credentials url opts))
+
+
+
+
+(defn search
+  [credentials params]
+  (if (valid-limit? (:limit params))
+    (-> (http-get credentials "https://oauth.reddit.com/search"
+                   {:query-params params})
+        (parse-response))))
+
+
+(defn frontpage
+  [credentials limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn controversial
+  [credentials limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/controversial/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn new
+  [credentials limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/new/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn rising
+  [credentials limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/rising/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn top
+  [credentials limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/top/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit
+  [credentials subreddit limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit-controversial
+  [credentials subreddit limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/controversial/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit-new
+  [credentials subreddit limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    
+    (-> (http-get credentials (str "https://oauth.reddit.com/r/" subreddit "/new/")
+                  {:query-params {:t time
+                                  :limit limit}})
+        #_(http-get credentials (str "https://www.reddit.com/r/" subreddit "/new/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit-rising
+  [credentials subreddit limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/rising/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit-top
+  [credentials subreddit limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/top/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn subreddit-comments
+  [credentials subreddit limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/comments/.json?limit=" limit))
+        (parse-response))))
+
+
+(defn subreddit-search
+  [credentials subreddit query limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://oauth.reddit.com/r/" subreddit "/search/" )
+                  {:query-params {:q query
+                                  :limit limit}})
+        (parse-response))))
+
+
+(defn subreddit-about
+  [credentials subreddit]
+  (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/about/.json"))
+      (parse-response)))
+
+(defn subreddit-moderators
+  [credentials subreddit]
+  (-> (http-get credentials (str "https://www.reddit.com/r/" subreddit "/about/moderators/.json"))
+      :data
+      :children))
+
+(defn subreddits
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/subreddits/.json?limit=" limit))
+        (parse-response))))
+
+(defn subreddits-new
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/subreddits/new/.json?limit=" limit))
+        (parse-response))))
+
+(defn subreddits-popular
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/subreddits/popular/.json?limit=" limit))
+        (parse-response))))
+
+(defn subreddits-gold
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/subreddits/gold/.json?limit=" limit))
+        (parse-response))))
+
+(defn subreddits-default
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/subreddits/default/.json?limit=" limit))
+        (parse-response))))
+
+(defn subreddits-search
+  [credentials subreddit limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://oauth.reddit.com/subreddits/search/")
+                  {:query-params {:q subreddit
+                                  :limit limit}})
+        (parse-response))))
+
+(defn user
+  [credentials username]
+  (-> (http-get credentials (str "https://www.reddit.com/user/" username "/about/.json"))
+      (parse-response)))
+
+(defn user-posts
+  [credentials username limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/user/" username "/submitted/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn user-comments
+  [credentials username limit time]
+  (if (and (valid-limit? limit) (valid-time? time))
+    (-> (http-get credentials (str "https://www.reddit.com/user/" username "/comments/.json?limit=" limit "&t=" (name time)))
+        (parse-response))))
+
+(defn user-trophies
+  [credentials username]
+  (-> (http-get credentials (str "https://www.reddit.com/user/" username "/trophies/.json"))
+      (parse-response)))
+
+(defn users
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/users/.json?limit=" limit))
+        (parse-response))))
+
+(defn users-new
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/users/new/.json?limit=" limit))
+        (parse-response))))
+
+(defn users-popular
+  [credentials limit]
+  (if (valid-limit? limit)
+    (-> (http-get credentials (str "https://www.reddit.com/users/popular/.json?limit=" limit))
+        (parse-response))))
+
+(defn listing
+  [credentials names]
+  (-> (http-get credentials (str "https://www.reddit.com/by_id/" (string/join "," names) "/.json"))
+      (parse-response)))
+
+
+(defn api-search-subreddits [credentials query]
+  (-> (http-post credentials "https://oauth.reddit.com/api/search_subreddits"
+                 {:form-params {:query query}})))
+
+
+
+(defn api-recommend-subreddits [credentials srnames]
+  (->> (http-get credentials (str "https://oauth.reddit.com/api/recommend/sr/"
+                                  (apply str (interpose "," srnames))))
+       (map :sr_name)))
+
+
 
 (defn init
   [credentials]
-  (let [response (client/get-access-token credentials)]
+  (let [response (get-access-token credentials)]
     (-> credentials
         (assoc :access-token (:access_token response))
-        (assoc :expires-in (+ (System/currentTimeMillis) (:expires_in response)))
-        (CredditClient.))))
+        (assoc :expires-in (+ (System/currentTimeMillis) (:expires_in response))))))
